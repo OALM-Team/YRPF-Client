e86dbb0e0e241e75867ca88dc1fa978a75820fd4
Nightwolf <nico.style931@gmail.com>
2020-06-17 00:58:41 +0200

    Welcome screen

--- ui/web/scripts/reducers/ui_modules.js
+++ ui/web/scripts/reducers/ui_modules.js
@@ -21,7 +21,7 @@ const initialState = {
     foodbar: true,
     phone: process.env.NODE_ENV == "development" ? true : false,
     houseBuy: false,
-    welcomeScreen: true,
+    welcomeScreen: process.env.NODE_ENV == "development" ? false : true,
     uiPosition: {
         inventory: {x: 400, y: 250},
         atm: {x: 400, y: 50},
